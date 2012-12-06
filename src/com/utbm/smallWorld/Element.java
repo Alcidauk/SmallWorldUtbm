3624dceff7a3fe2483f9848f942c444d53761d38
Tiller <git.me@tiller.fr>
2012-12-06 17:09:41 +0100

    Element

---
+++ src/com/utbm/smallWorld/Element.java
@@ -0,0 +1,5 @@
+package com.utbm.smallWorld;
+
+public class Element {
+	
+}
--- src/com/utbm/smallWorld/Territoire.java
+++ src/com/utbm/smallWorld/Territoire.java
@@ -1,10 +1,137 @@
 package com.utbm.smallWorld;
 
-public class Territoire {
+import java.util.LinkedList;
+import java.util.List;
 
-	public int getNbUnite() {
-		// TODO Auto-generated method stub
+/**
+ * Représentation d'un Territoire
+ * 
+ * @author LONGO Michael
+ * @version 1.0
+ */
+public class Territoire {
+	private int nbUnite = 0;
+	
+	private Peuple occupant = null;
+	
+	private boolean estEnBordure = false;
+
+	private List<Territoire> territoiresAdjacents;
+	private List<Element> elements;
+	
+	public Territoire() {
+		territoiresAdjacents = new LinkedList<Territoire>();
+		elements = new LinkedList<Element>();
+	}
+	
+	public Territoire(boolean enBordure) {
+		this();
+		
+		this.estEnBordure = enBordure;
+	}
+	
+	public int coutAttaque() {
+		int cout = this.nbUnite;
+		
 		return 0;
+	}
+	
+	public int bonusAttaque() {
+		return 0;
+	}
+	
+	public int bonusGain() {
+		return 0;
+	}
+	
+	public void ajouterUnite(int nbUnite) {
+		this.nbUnite += nbUnite;
+	}
+	
+	public void retirerUnite(int nbUnite) {
+		this.nbUnite -= nbUnite;
+	}
+	
+	public boolean estAdjacent(Peuple p) {
+		return false;
+	}
+	
+	public void priseTerritoire(Peuple attaquant, int nbUnite) {
+		
+	}
+	
+	public void abandon() {
+		
+	}
+
+	/**
+	 * @return the nbUnite
+	 */
+	public int getNbUnite() {
+		return nbUnite;
+	}
+
+	/**
+	 * @return the occupant
+	 */
+	public Peuple getOccupant() {
+		return occupant;
+	}
+
+	/**
+	 * @return the estEnBordure
+	 */
+	public boolean isEstEnBordure() {
+		return estEnBordure;
+	}
+
+	/**
+	 * @return the territoiresAdjacents
+	 */
+	public List<Territoire> getTerritoiresAdjacents() {
+		return territoiresAdjacents;
+	}
+
+	/**
+	 * @return the elements
+	 */
+	public List<Element> getElements() {
+		return elements;
+	}
+
+	/**
+	 * @param nbUnite the nbUnite to set
+	 */
+	public void setNbUnite(int nbUnite) {
+		this.nbUnite = nbUnite;
+	}
+
+	/**
+	 * @param occupant the occupant to set
+	 */
+	public void setOccupant(Peuple occupant) {
+		this.occupant = occupant;
+	}
+
+	/**
+	 * @param estEnBordure the estEnBordure to set
+	 */
+	public void setEstEnBordure(boolean estEnBordure) {
+		this.estEnBordure = estEnBordure;
+	}
+
+	/**
+	 * @param territoiresAdjacents the territoiresAdjacents to set
+	 */
+	public void setTerritoiresAdjacents(List<Territoire> territoiresAdjacents) {
+		this.territoiresAdjacents = territoiresAdjacents;
+	}
+
+	/**
+	 * @param elements the elements to set
+	 */
+	public void setElements(List<Element> elements) {
+		this.elements = elements;
 	}
 	
 }
