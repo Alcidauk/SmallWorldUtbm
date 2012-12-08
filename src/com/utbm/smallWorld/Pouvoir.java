00b9eb0778ead935d2fa6e4de721ee4e986f9b00
aukman <piR.R25@gmail.com>
2012-12-08 12:32:51 +0000

    début pouvoir, package pouvoirs

--- src/com/utbm/smallWorld/Pouvoir.java
+++ src/com/utbm/smallWorld/Pouvoir.java
@@ -2,8 +2,57 @@ package com.utbm.smallWorld;
 
 import com.utbm.smallWorld.interfaces.Bonusable;
 
-public class Pouvoir implements Bonusable {
+public abstract class Pouvoir implements Bonusable {
+	
+	/** nombre de pions de peuples qu'apporte le pouvoir en plus */
+	protected int nbUniteApporte = 0;
+	
+	/** peuple auquel est lié le pouvoir spécial, null si lié à aucun. */
+	protected Peuple peupleLie = null;
+	
+	protected boolean actifEnDeclin = false;
 
+	
+	/**
+	 * constructeur
+	 * permet d'instancier les pouvoirs au début.
+	 * @param nbUnite nombre d'unités qu'apporte le pouvoir
+	 */
+	public Pouvoir(int nbUnite, boolean actifDeclin){
+		this.nbUniteApporte = nbUnite;
+		this.actifEnDeclin = actifDeclin;
+	}
+	
+	/* accesseurs peupleLie */
+	
+	/**
+	 * setter du peupleLie
+	 * @param peupleALier le peuple à associer au pouvoir
+	 */
+	public void setPeupleLie(Peuple peupleALier){
+		this.peupleLie = peupleALier;
+	}
+	
+	
+	/**
+	 * getter du peupleLie
+	 * @return retourne le peuple lié au pouvoir dans la partie
+	 */
+	public Peuple getPeupleLie(){
+		return this.peupleLie;
+	}
+	
+	/* accesseurs actifEnDeclin */
+	
+	/**
+	 * getter d'actifEnDeclin
+	 * @return retourne actifEnDeclin
+	 */
+	public boolean getActifEnDeclin(){
+		return this.actifEnDeclin;
+	}
+	
+	
 	@Override
 	public int bonusGain(Territoire t) {
 		// TODO Auto-generated method stub
