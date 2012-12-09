1f2f187502d9112f36114886dbb29548566cc292
aukman <piR.R25@gmail.com>
2012-12-09 16:02:56 +0000

    Implémentation de la classe Partie

--- src/com/utbm/smallWorld/Partie.java
+++ src/com/utbm/smallWorld/Partie.java
@@ -1,10 +1,64 @@
 package com.utbm.smallWorld;
 
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+
 public class Partie {
+	
+	private static Partie part;
+	
+	protected int nbTours = 10;
+	
+	protected int tourEnCours = 0;
+	
+	protected List<Joueur> lstJoueurs = null;
+	
+	protected Joueur joueurEnCours = null;
+	
+	protected List<Peuple> peuplesPris = null;
+	
+	protected List<Peuple> peuplesDispo = null;
+	
+	protected List<Pouvoir> pouvoirsPris = null;
+	
+	protected List<Pouvoir> pouvoirsDispo = null;
+	
+	private Partie(){
+		lstJoueurs = new LinkedList<Joueur>();
+		peuplesPris = new LinkedList<Peuple>();
+		peuplesDispo = new LinkedList<Peuple>();
+		pouvoirsPris = new LinkedList<Pouvoir>();
+		pouvoirsDispo = new LinkedList<Pouvoir>();
+		
+		initPeuples();
+		initPouvoirs();
+	}
+	
+	public static Partie getInstance(){
+		if( part == null )
+			part = new Partie();
+		return part;
+	}
 
 	public static void remettreBoite(Peuple peuple) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void ajouterJoueur(Joueur j){
+		lstJoueurs.add(j);
+	}
+	
+	public void initPeuples(){
+		
+	}
+	
+	public void initPouvoirs(){
+		
+	}
+	
 	
 }
--- src/com/utbm/smallWorld/pouvoirs/PouvoirAvare.java
+++ src/com/utbm/smallWorld/pouvoirs/PouvoirAvare.java
@@ -1,6 +1,7 @@
 package com.utbm.smallWorld.pouvoirs;
 
 import com.utbm.smallWorld.Pouvoir;
+import com.utbm.smallWorld.Territoire;
 
 public class PouvoirAvare extends Pouvoir {
 	
@@ -9,6 +10,14 @@ public class PouvoirAvare extends Pouvoir {
 	public PouvoirAvare(int nbUnite, boolean actifDeclin) {
 		super(nbUnite, actifDeclin);
 		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @return
+	 */
+	public int bonusGain(Territoire t) {
+		//if( this.peupleLie)
+		return 0;
 	}
 	
 	
