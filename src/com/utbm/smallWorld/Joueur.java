8c42f1742afbd3cb2a990fbfa37f483492aac2d3
amani12 <amani.younes2@gmail.com>
2012-12-14 17:31:48 +0100

    Update src/com/utbm/smallWorld/Joueur.java
    classe du joueur

--- src/com/utbm/smallWorld/Joueur.java
+++ src/com/utbm/smallWorld/Joueur.java
@@ -8,20 +8,57 @@ public class Joueur {
 	private String nom;
 	private int argent;
 	private Peuple peuple ;
-	private LinkedList<Peuple> peuplesDeclin;
+	private LinkedList<Peuple> peuplesDeclin; /*je suis pas sure si c'est une linkedlist ou un tableau*/
 	
-	
+	// Constructer
 	public Joeur()
 	{
 		nom= null;
 		argent=0;
-		peuple= null;
-		this.peuplesDeclin= new LinkedList <Peuple>
+		peuple= new Peuple(null);
+		peuplesDeclin= null;
 	}
-	public Joeur(string n, int monnaie, )
+	public Joeur(string n, int monnaie, Peuple p)
+	{
+		nom= n;
+		argent= monnaie;
+		peuple =new Peuple(p);
+		this.peuplesDeclin= new LinkedList<Peuple>;  
+		
+	}
+	
+	// Fonctions Setter
+	public void setNom(string n)
+	{
+		nom = n;
+	}
+	public void setArgent(int n)
+	{
+		argent = n;
+	}
+	public void setPeuple(Peuple p)
+	{
+		peuple = new peuple(p);
+	}
+	
+	//Fonctions Getter
+	public String getNom()
+	{
+		return nom;
+	}
+	public int getArgent()
+	{
+		return argent;
+	}
+	//Fonction Attaquer un Territoire
+	public void attaquer(Territoire t)
+	{
+			
+	}
 	public void pertePeuple(Peuple peuple) {
 		// TODO Auto-generated method stub
 		
 	}
 	
 }
+
