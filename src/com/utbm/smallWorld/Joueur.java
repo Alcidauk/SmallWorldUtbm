3b7409cba06d1a8d6d9f57ca3132eb586bad2add
amani12 <amani.younes2@gmail.com>
2012-12-14 16:24:31 +0100

    Update src/com/utbm/smallWorld/Joueur.java

--- src/com/utbm/smallWorld/Joueur.java
+++ src/com/utbm/smallWorld/Joueur.java
@@ -1,7 +1,13 @@
 package com.utbm.smallWorld;
-
+#include 
 public class Joueur {
 
+	private String nom;
+	private int argent;
+	private Peuple peuple = null;
+	private LinkedList<Peuple> peuplesDeclin;
+	
+	
 	public void pertePeuple(Peuple peuple) {
 		// TODO Auto-generated method stub
 		
