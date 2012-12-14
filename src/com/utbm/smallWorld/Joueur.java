75f7040fac8abb6ee087587894dd902f2b4d779a
amani12 <amani.younes2@gmail.com>
2012-12-14 16:34:48 +0100

    Update src/com/utbm/smallWorld/Joueur.java

--- src/com/utbm/smallWorld/Joueur.java
+++ src/com/utbm/smallWorld/Joueur.java
@@ -1,13 +1,24 @@
 package com.utbm.smallWorld;
-#include 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+
 public class Joueur {
 
 	private String nom;
 	private int argent;
-	private Peuple peuple = null;
+	private Peuple peuple ;
 	private LinkedList<Peuple> peuplesDeclin;
 	
 	
+	public Joeur()
+	{
+		nom= null;
+		argent=0;
+		peuple= null;
+		this.peuplesDeclin= new LinkedList <Peuple>
+	}
+	public Joeur(string n, int monnaie, )
 	public void pertePeuple(Peuple peuple) {
 		// TODO Auto-generated method stub
 		
