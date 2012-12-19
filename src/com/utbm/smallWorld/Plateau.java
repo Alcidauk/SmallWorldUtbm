a229f42d013f841a56d5f0f7f82d23d78dc40e7f
Tiller <github.me@tiller.fr>
2012-12-19 21:23:18 +0100

    Plateau*

---
+++ src/com/utbm/smallWorld/Plateau.java
@@ -0,0 +1,28 @@
+package com.utbm.smallWorld;
+
+import java.util.List;
+
+import javax.swing.Icon;
+
+public class Plateau {
+	/** Image du plateau */
+	private Icon image;
+	
+	/** Territoires du plateau */
+	private List<Territoire> territoires;
+	
+	/**
+	 * Constructeur
+	 */
+	public Plateau(Icon image) {
+		
+	}
+	
+	public Icon getIcon() {
+		return image;
+	}
+	
+	public List<Territoire> getTerritoires() {
+		return territoires;
+	}
+}
