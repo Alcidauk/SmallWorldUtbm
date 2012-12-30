74222d81b6771870b9b848b6da997ea1001d4e6e
Administrateur <Administrateur@Tiller-PC>
2012-12-30 12:05:13 +0100

    Message de transition²

---
+++ src/com/utbm/smallWorld/gui/WinWait.java
@@ -0,0 +1,74 @@
+package com.utbm.smallWorld.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class WinWait extends JDialog implements MouseListener {
+	public WinWait(String mess) {
+		setModal(true);
+		setBounds(Game.getInstance().getBounds());
+		setResizable(false);
+		setUndecorated(true);
+
+		// Fond d'écran
+		JLabel background = new JLabel();
+		background.setIcon(Game.BACKGROUND);
+		FlowLayout layBack = new FlowLayout();
+		layBack.setVgap(0);
+		background.setLayout(layBack);
+		
+
+		
+		JPanel infoTemp = new JPanel();
+		infoTemp.setBounds(0, 0, 1274, 740);
+		infoTemp.setPreferredSize(new Dimension(1274, 740));
+		infoTemp.setOpaque(false);
+		
+		JLabel labInfoTemp = new JLabel(mess);
+		labInfoTemp.setForeground(Color.WHITE);
+		labInfoTemp.setFont(new Font("sherif", 0, 60));
+		labInfoTemp.setVerticalAlignment(JLabel.CENTER);
+		labInfoTemp.setHorizontalAlignment(JLabel.CENTER);
+		labInfoTemp.setPreferredSize(new Dimension(1274, 740));
+		labInfoTemp.setOpaque(false);
+		
+		infoTemp.add(labInfoTemp);
+
+		setContentPane(background);
+		
+		getContentPane().add(infoTemp);
+		
+		final JDialog that = this;
+		
+		new Thread(new Runnable(){
+			public void run() {
+				try {
+					Thread.sleep(2000);
+				}
+				catch (Exception e) {}
+				
+				that.setVisible(false);
+			}
+		}).start();
+		
+		setVisible(true);
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		setVisible(false);
+	}
+	
+	public void mouseClicked(MouseEvent arg0) {}
+	public void mouseEntered(MouseEvent arg0) {}
+	public void mouseExited(MouseEvent arg0) {}
+	public void mouseReleased(MouseEvent arg0) {}
+}
